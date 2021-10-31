fbb32df95a8540e312fcf7a2c240a0729c117671
FAXec <xecfahim.ahmed@gmail.com>
2021-10-31 17:15:59 +0000

    updated checkWin function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const getKeyboard = document.getElementById("qwerty");
 const getPhrase = document.getElementById("phrase");
 const startButton = document.querySelector(".btn__reset");
 const overlay = document.getElementById("overlay");
+let heart = document.querySelectorAll("img");
+
 
 
 let missed = 0;
@@ -17,16 +19,40 @@ let phrases = ["Quick and Dirty",
                 "Beating Around the Bush", 
                 "When the Rubber Hits the Road"];
 
+
+                /*Pseudocode for getRandomPhraseAsArray(arr)
+  create a random number between 0 and arr.length
+  use the random number as an index to select a string from arr
+  convert the characters into lower case
+  split the chosen string into an array
+  return the array
+*/
+
                 //The function declaration chooses random phrases from phrases array
 function getRandomPhraseAsArray(arr) {
     const randomNumber = Math.floor(Math.random() * arr.length);
-    const selectPhrase = arr[randomNumber];
+    const selectPhrase = arr[randomNumber].toLowerCase();
     return selectPhrase.split("");
     
  };
 
+//  console.log(getRandomPhraseAsArray(phrases));
+
  const randomPhrase = getRandomPhraseAsArray(phrases)
 
+/*
+  create a addPhraseToDisplay function
+  create a loop for each character
+  create li item list
+  add character to the list
+  append list to ul 
+  if condition
+  character in array is letter and not space 
+  function should add class "letter" to list item
+  else "space"
+  save value returned by getRandomPhraseAsArray to a variable 
+  pass as an arguement to addPhraseToDisplay 
+ */
 
 
  function addPhraseToDisplay(arr) {
@@ -91,14 +117,14 @@ const randomDisplay = addPhraseToDisplay(randomPhrase);
     let buttonPressed = event.target;
     if (buttonPressed.tagName === "BUTTON" || buttonPressed.className === "chosen") {
 
-        buttonPressed.classList.add = "chosen";
+        buttonPressed.classList.add("chosen");
         buttonPressed.disabled = true;
 
         let letterFound = checkLetter(buttonPressed);
         // console.log(letterFound);
 
         if (letterFound == false) {
-            let heart = document.querySelectorAll("img");
+            
             heart[missed].src = "images/lostHeart.png";
             missed++;
         }
@@ -109,18 +135,34 @@ const randomDisplay = addPhraseToDisplay(randomPhrase);
 
 /*Pseudocode checkWin function
     create variable and store li elements with class name "letter"
-    create variable and storeli elements with class name "show"
-
-  
-
-
-
-
-
+    create variable and store li elements with class name "show"
+    if condition
+    create win overlay and add class "win" to it
+    change the headline text to "You Win"
+    change display property to flex
+    if condition for missed counter greater than 4
+    create overlay by adding "lose" class
+    change the headline text to lost
+    change display property to overlay "flex"
 */
 
+const winLetter = document.getElementsByClassName("letter");
+//console.log(winLetter)
+const winShow = document.querySelectorAll(".show");
+//console.log(winShow);
 
 
+function checkWin() {    
+    if(winLetter.length === winShow.length){              
+        overlay.classList.add("win");
+        overlay.textContent = "Congratulations, you win";
+        overlay.style.display = "flex";
+    }else if (missed >= 5 && winLetter !== winShow) {
+        overlay.classList.add("lose");
+        overlay.textContent = "You lose, try again";
+        overlay.style.display = "flex";        
+    }
+}
 
 
 
